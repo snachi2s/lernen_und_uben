4ef218b9ea2ad846820520a28c5ad6bb38ffc478
Selva <91877888+snachi2s@users.noreply.github.com>
2026-08-22 18:37:47 +0200

    [update] verbessert version mit pointers und fehler handlung

--- C++/3D-zu-2D_projektion.cpp
+++ C++/3D-zu-2D_projektion.cpp
@@ -1,24 +1,45 @@
 #include<iostream>
 using namespace std;
+#include<stdexcept>
 
 struct Point2D{
-    float u,v;
+    double u;
+    double v;
 };
 
 struct Point3D{
-    float X, Y, Z;
+    double X;
+    double Y;
+    double Z;
 };
 
 struct CameraParams{
-    float focal_x, focal_y, cx, cy;
+    double focal_x;
+    double focal_y;
+    double cx;
+    double cy;
 };
 
-Point2D project3Dto2D(Point3D worldPoints){
+
+Point2D project3Dto2D(const Point3D& worldPoints){ 
+    //worldpoints here is reference --> if we dont want to modify the object (if we know the values are fixed throughout the function)
     CameraParams intrinsicValues = {800, 800, 320, 240};
+
+    if(worldPoints.Z <= 0){
+        throw invalid_argument("Z not valid");
+    }
+
+    //pass by reference
     Point2D imgPoints;
     imgPoints.u = (intrinsicValues.focal_x * (worldPoints.X/worldPoints.Z)) + intrinsicValues.cx;
     imgPoints.v = (intrinsicValues.focal_y * (worldPoints.Y/worldPoints.Z)) + intrinsicValues.cy;
     return imgPoints;
+
+    //pass by value
+    double u = (intrinsicValues.focal_x * (worldPoints.X/worldPoints.Z)) + intrinsicValues.cx;
+    double v = (intrinsicValues.focal_y * (worldPoints.Y/worldPoints.Z)) + intrinsicValues.cy;
+    return {u, v};
+
 }
 
 int main(){
@@ -26,7 +47,7 @@ int main(){
     //variables
     Point3D worldPoints;
 
-    cout << "Geben Sie die 3D Punkte ein"<<endl;
+    cout << "Geben Sie den 3D Punkte ein"<<endl;
     cout<<"X:";
     cin>>worldPoints.X;
     cout<<"Y:";
@@ -34,10 +55,15 @@ int main(){
     cout<<"Z:";
     cin>>worldPoints.Z;
 
-    Point2D imgPoints = project3Dto2D(worldPoints);
-    cout<<"Die 2D-Koordinaten sind: "<<endl;
-    cout<<"u: "<<imgPoints.u<<endl;
-    cout<<"v: "<<imgPoints.v<<endl;
+    try{
+        Point2D imgPoints = project3Dto2D(worldPoints);
+        cout<<"Die 2D-Koordinaten sind: "<<endl;
+        cout<<"u: "<<imgPoints.u<<endl;
+        cout<<"v: "<<imgPoints.v<<endl;
+    }
+    catch(const invalid_argument& e){
+        cout << "Fehler: " << e.what() << endl;
+    }
     
     return 0;
 }
